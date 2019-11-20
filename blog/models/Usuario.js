92c634f300569d45986745e441d1644e311317af
Mike-Fernandez <00145518@uca.edu.sv>
2019-11-20 00:18:17 -0600

    Added usuario model

---
+++ blog/models/Usuario.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var userSchema = Schema({
+    username: {type: String, required: true, unique: true},
+    first_name: String,
+    last_name: String,
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    login_count: Number
+},{
+    timestamps: true
+});
+
+module.exports = mongoose.model("User", userSchema);
